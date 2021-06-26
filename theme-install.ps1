6a523cb6b58d78f60f3d823a5d968631037b572d
Daksh P. Jain <jaindaksh42@gmail.com>
2021-06-26 21:51:42 +0530

    Update theme-install.ps1

--- theme-install.ps1
+++ theme-install.ps1
@@ -4,21 +4,20 @@ Website: https://daksh.eu.org
 '@`n
 
 Add-Type -AssemblyName PresentationFramework
-$input = [System.Windows.MessageBox]::Show('Is Spicetify CLI installed in your system?', 'Spicetify CLI Installtion', 'YesNoCancel');
+$spice = [System.Windows.MessageBox]::Show('Is Spicetify CLI installed in your system?', 'Spicetify CLI Installtion', 'YesNoCancel');
 
-if ($input -eq 'Yes') {
+if ($spice -eq 'Yes') {
  Write-Host "`nSkipping Spicetify installation`n"
  Write-Host "Installing theme`n"
  spicetify upgrade
  cd "$(spicetify -c | Split-Path)\Themes"
  git clone https://github.com/Daksh777/SpotifyNoPremium
  spicetify config current_theme SpotifyNoPremium
- spicetify apply
+ spicetify restore backup apply
  Write-Host "`nInstalled successfully"
- Read-Host "Press enter to exit"
  }
 
- if ($input -eq 'No') {
+ if ($spice -eq 'No') {
  Write-Host "`nInstalling Spicetify CLI`n"
  Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
  Write-Host "`n Installed Spicetify CLI`n"
@@ -26,12 +25,31 @@ if ($input -eq 'Yes') {
  cd "$(spicetify -c | Split-Path)\Themes"
  git clone https://github.com/Daksh777/SpotifyNoPremium
  spicetify config current_theme SpotifyNoPremium
- spicetify apply
+ spicetify restore backup apply
  Write-Host "`n Installed theme successfully"
- Write-Host "`n Press enter to exit"
  }
 
- if ($input -eq 'Cancel') {
+ if ($spice -eq 'Cancel') {
  Write-Host "`nOperation Cancelled"
-exit
+ exit
+}
+
+
+Add-Type -AssemblyName PresentationFramework
+$bts = [System.Windows.MessageBox]::Show('Do you want to install BlockTheSpot to block ads? (recommended)', 'BlockTheSpot Installation', 'YesNoCancel');
+
+if ($bts -eq 'Yes') {
+Invoke-WebRequest -Uri "https://github.com/Daksh777/BlockTheSpot/raw/master/SpotifyNoPremium.bat" -OutFile "SpotifyNoPremium.bat"
+cmd.exe /c ".\SpotifyNoPremium.bat"
+Remove-Item "SpotifyNoPremium.bat"
+}
+
+if ($bts -eq 'No') {
+ Write-Host "`nNot installing BlockTheSpot"
+ exit
+ }
+
+ if ($spice -eq 'Cancel') {
+ Write-Host "`nOperation Cancelled"
+ exit
 }
