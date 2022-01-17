60766f8f43ae5e918327e3010e895ac67278dabc
Daksh P. Jain <jaindaksh42@gmail.com>
2022-01-17 15:41:26 +0530

    Update update.bat

--- update.bat
+++ update.bat
@@ -12,9 +12,23 @@ Website: https://daksh.eu.org
 
 Add-Type -AssemblyName Microsoft.VisualBasic
 
-Write-Host "Updating theme from GitHub repo"
-Set-Location "$(spicetify -c | Split-Path)\Themes\SpotifyNoPremium"
-git pull origin
+$css = [Microsoft.VisualBasic.Interaction]::MsgBox('If you have modified any files (like the user.css or color.ini), make sure to back it up right now or it will get deleted. Continue?', 'YesNo,MsgBoxSetForeground,Question,SystemModal,DefaultButton1', 'Backup custom CSS');
+if ($css -eq 'No') {
+    Write-Host "Please backup any custom files and run this script again"
+    exit
+    }
+
+Get-ChildItem "$(spicetify -c | Split-Path)\Themes\SpotifyNoPremium" -Exclude .git | Remove-Item -Force
+
+Write-Host 'Downloading files from GitHub repository'
+Set-Location C:\Temp
+Invoke-WebRequest -Uri 'https://github.com/Daksh777/SpotifyNoPremium/archive/main.zip' -OutFile 'temp.zip'
+Expand-Archive 'temp.zip'
+Remove-Item 'temp.zip'
+Rename-Item -Path temp/SpotifyNoPremium-main -NewName SpotifyNoPremium
+Get-ChildItem temp/SpotifyNoPremium | Copy-Item -Destination "$(spicetify -c | Split-Path)\Themes\SpotifyNoPremium" -Force -Recurse
+Remove-Item temp -Recurse -Force
+Write-Host "`nDownloaded successfully"
 
 Write-Host "`nUpdating Spicetify"
 spicetify upgrade
