9abed42e8777de09041b1197dfe5794266ce6523
Daksh P. Jain <jaindaksh42@gmail.com>
2024-05-22 21:13:21 +0530

    Update adblock
    https://github.com/rxri/spicetify-extensions/commit/34130a54cd7a4b1b953cd9c1863232c9f28514e9

--- adblock.js
+++ adblock.js
@@ -3,11 +3,20 @@ const loadWebpack = () => {
     try {
         const require = window.webpackChunkclient_web.push([[Symbol()], {}, (re) => re]);
         const cache = Object.keys(require.m).map(id => require(id));
-        return cache;
+        const modules = cache
+            .filter(module => typeof module === "object")
+            .flatMap(module => {
+            try {
+                return Object.values(module);
+            }
+            catch { }
+        });
+        const functionModules = modules.filter(module => typeof module === "function");
+        return { cache, functionModules };
     }
     catch (error) {
         console.error("adblockify: Failed to load webpack", error);
-        return [];
+        return { cache: [], functionModules: [] };
     }
 };
 const getSettingsClient = (cache) => {
@@ -18,6 +27,27 @@ const getSettingsClient = (cache) => {
         console.error("adblockify: Failed to get ads settings client", error);
         return null;
     }
+};
+const getSlotsClient = (functionModules, transport) => {
+    try {
+        const slots = functionModules.find(m => m.SERVICE_ID === "spotify.ads.esperanto.slots.proto.Slots" || m.SERVICE_ID === "spotify.ads.esperanto.proto.Slots");
+        return new slots(transport);
+    }
+    catch (error) {
+        console.error("adblockify: Failed to get slots client", error);
+        return null;
+    }
+};
+const map = new Map();
+const retryCounter = (slotId, action) => {
+    if (!map.has(slotId))
+        map.set(slotId, { count: 0 });
+    if (action === "increment")
+        map.get(slotId).count++;
+    else if (action === "clear")
+        map.delete(slotId);
+    else if (action === "get")
+        return map.get(slotId)?.count;
 };
 (async function adblockify() {
     // @ts-expect-error: Events are not defined in types
@@ -43,7 +73,7 @@ const getSettingsClient = (cache) => {
             await productState.putOverridesValues({ pairs: { ads: "0", catalogue: "premium", product: "premium", type: "premium" } });
         }
         catch (error) {
-            console.error("adblockify: Failed inside `disableAds` function", error);
+            console.error("adblockify: Failed inside `disableAds` function\n", error);
         }
     };
     const configureAdManagers = async () => {
@@ -65,7 +95,7 @@ const getSettingsClient = (cache) => {
             setTimeout(disableAds, 100);
         }
         catch (error) {
-            console.error("adblockify: Failed inside `configureAdManagers` function", error);
+            console.error("adblockify: Failed inside `configureAdManagers` function\n", error);
         }
     };
     const bindToSlots = async () => {
@@ -77,18 +107,29 @@ const getSettingsClient = (cache) => {
     const handleAdSlot = (data) => {
         const slotId = data?.adSlotEvent?.slotId;
         try {
-            audio.inStreamApi.adsCoreConnector.clearSlot(slotId);
+            const adsCoreConnector = audio.inStreamApi.adsCoreConnector;
+            if (typeof adsCoreConnector?.clearSlot === "function")
+                adsCoreConnector.clearSlot(slotId);
+            const slotsClient = getSlotsClient(webpackCache.functionModules, productState.transport);
+            if (slotsClient)
+                slotsClient.clearAllAds({ slotId });
             updateSlotSettings(slotId);
         }
         catch (error) {
-            console.error("adblockify: Failed inside `handleAdSlot` function. Retrying in 100ms...", error);
-            setTimeout(handleAdSlot, 100, data);
+            console.error("adblockify: Failed inside `handleAdSlot` function. Retrying in 1 second...\n", error);
+            retryCounter(slotId, "increment");
+            if (retryCounter(slotId, "get") > 5) {
+                console.error(`adblockify: Failed inside \`handleAdSlot\` function for 5th time. Giving up...\nSlot id: ${slotId}.`);
+                retryCounter(slotId, "clear");
+                return;
+            }
+            setTimeout(handleAdSlot, 1 * 1000, data);
         }
         configureAdManagers();
     };
     const updateSlotSettings = async (slotId) => {
         try {
-            const settingsClient = getSettingsClient(webpackCache);
+            const settingsClient = getSettingsClient(webpackCache.cache);
             if (!settingsClient)
                 return;
             await settingsClient.updateStreamTimeInterval({ slotId, timeInterval: "0" });
@@ -96,7 +137,7 @@ const getSettingsClient = (cache) => {
             await settingsClient.updateDisplayTimeInterval({ slotId, timeInterval: "0" });
         }
         catch (error) {
-            console.error("adblockify: Failed inside `updateSlotSettings` function.", error);
+            console.error("adblockify: Failed inside `updateSlotSettings` function\n", error);
         }
     };
     const intervalUpdateSlotSettings = async () => {
@@ -109,7 +150,7 @@ const getSettingsClient = (cache) => {
             audio.inStreamApi.adsCoreConnector.subscribeToSlot(slot, handleAdSlot);
         }
         catch (error) {
-            console.error("adblockify: Failed inside `subToSlot` function", error);
+            console.error("adblockify: Failed inside `subToSlot` function\n", error);
         }
     };
     const runObserver = () => {
@@ -145,14 +186,14 @@ const getSettingsClient = (cache) => {
             const inAppMessages = expFeatures.enableInAppMessaging?.value;
             const upgradeCTA = expFeatures.hideUpgradeCTA?.value;
             if (!hptoEsperanto)
-                expFeatureOverride({ name: "enableEsperantoMigration", default: true });
+                expFeatureOverride({ type: "bool", name: "enableEsperantoMigration", default: true });
             if (inAppMessages)
-                expFeatureOverride({ name: "enableInAppMessaging", default: false });
+                expFeatureOverride({ type: "bool", name: "enableInAppMessaging", default: false });
             if (!upgradeCTA)
-                expFeatureOverride({ name: "hideUpgradeCTA", default: true });
+                expFeatureOverride({ type: "bool", name: "hideUpgradeCTA", default: true });
         }
         catch (error) {
-            console.error("adblockify: Failed inside `enableExperimentalFeatures` function", error);
+            console.error("adblockify: Failed inside `enableExperimentalFeatures` function\n", error);
         }
     };
     enableExperimentalFeatures();
